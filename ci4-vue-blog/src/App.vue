e61c80104f84b7292c6ea6ff71234fa9fc538b3c
tquangdo <trangia61@gmail.com>
2021-02-15 20:17:15 +0900

    20210215 20:17

---
+++ ci4-vue-blog/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app-vue">
+    <!-- components: {appNav: Nav,} -->
+    <app-nav></app-nav>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from "./cmps/Nav";
+export default {
+  name: "App",
+  components: {
+    appNav: Nav, // <app-nav>
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ ci4-vue-blog/src/Post.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Single Post</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
